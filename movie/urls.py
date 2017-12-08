fa1b374440e63758107744f39994c64dc46c0418
siddhism <siddhesh.it@gmail.com>
2017-12-08 15:51:56 +0530

    static fix

--- movie/urls.py
+++ movie/urls.py
@@ -23,7 +23,6 @@ urlpatterns = [
     url(r'^api/', include(movie_urls, namespace='api')),
 ]
 
-if not settings.DEBUG:
-    urlpatterns += patterns('',
-        (r'^static/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.STATIC_ROOT}),
-    )
+urlpatterns += patterns('',
+    (r'^static/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.STATIC_ROOT}),
+)
